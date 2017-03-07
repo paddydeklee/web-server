33d37c31c9d84d930911b2a67517f1b971de0dc9
paddydeklee <paddydeklee@gmail.com>
2017-03-07 21:44:10 +0000

    updated port for heroku

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 var express = require('express')
 var app = express()
-var PORT = 3000
+var PORT = process.env.PORT || 3000
 var middleware = require('./middleware.js')
 
-// app.use(middleware.logger)
+app.use(middleware.logger)
 
 app.get('/about', middleware.requireAuthentication, function(req, res){
   res.send('About Us')
